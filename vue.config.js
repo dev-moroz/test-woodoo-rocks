6239cf0d9f156a92333db79bf95434479acbfed4
dev-moroz <dev_moroz@mail.ru>
2022-09-02 10:40:34 +0300

    add publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/woodoo-rocks/",
+  publicPath: "/test-woodoo-rocks/",
 });
