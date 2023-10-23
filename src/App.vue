767f20d0011fe80c7aaf0f0441a4ddf7712e39d6
Anatoly Morozov <dev_moroz@mail.ru>
2023-10-23 21:07:31 +0200

    feat: add endless scroll

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
 <template>
   <div class="container">
-    <div class="input-group input-group-sm mb-3 mt-4 mx-auto w-75" style="gap:8px">
-      <div class="input-group-prepen">
-        <span class="input-group-text" id="inputGroup-sizing-sm">Search</span>
-      </div>
-      <input type="text" class="form-control rounded-2" aria-describedby="inputGroup-sizing-sm" v-model="filter">
+    <div class="input-search my-3 m-auto d-flex">
+      <label for="search">Search: </label>
+      <input id="search" type="text" v-model="filter" class="w-100">
     </div>
     <template v-if="isLoaded">
       <div v-if="nothingFound">
         <p class="text-center">Nothing found</p>
       </div>
-      <div class="d-flex flex-row flex-wrap align-content justify-content-center mt-2" v-else>
-        <div v-for="item in sortPosts" :key="item.id" class="m-2">
+      <div class="d-flex flex-row flex-wrap mt-2 justify-content-between;"  v-else>
+        <div v-for="item in sortPosts" :key="item.id" class="mx-2 my-1" style="flex-basis: 31%;">
           <AppPost :data="item" />
         </div>
       </div>
     </template>
-    <div class="text-center mt-3" v-else>
+    <div class="text-center my-5" v-if="isLoadData">
       <div class="spinner-grow" role="status">
         <span class="sr-only"></span>
       </div>
     </div>
-
 
   </div>
 </template>
@@ -34,14 +31,25 @@ export default {
   data(){
     return{
       posts: [],
-      filter:''
+      filter:'',
+      showedPosts: [],
+      slice: 10,
+      isLoadData: false
     }
   },
   components: {
     AppPost
   },
+  watch: {
+    isLoadData: {
+      handler(value){
+        if(value) setTimeout(() => this.setPartData(), 2000)
+      }
+    }
+  },
   methods: {
     async getAllData(){
+      this.isLoadData = true
       const posts = await fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response => response.json())
       const users = await fetch('https://jsonplaceholder.typicode.com/users')
@@ -51,23 +59,54 @@ export default {
         const author = users.find(user => user.id === post.userId).name
         return {...post, author: author}
         })
+
+      setTimeout(() => this.setPartData(), 1000)
+    },
+
+    setPartData(){
+      this.showedPosts = this.posts.slice(0, this.slice)
+      this.slice += 10
+      this.isLoadData = false
+    },
+
+    loadData(){        
+      const scrolled = window.scrollY
+      const height = document.body.offsetHeight
+      const screenHeight = window.innerHeight
+      const partOfHeight = (height - screenHeight) - 100
+
+      if(scrolled > partOfHeight) this.isLoadData = true
     }
   },
   computed:{
     sortPosts(){
-      return this.filter.length>0
-        ? this.posts.filter(item => item.author.toLowerCase().includes(this.filter.toLowerCase()))
-        : this.posts
+      const filter = this.filter.toLowerCase()
+      return this.filter.length
+        ? this.showedPosts.filter(post => post.title.toLowerCase().includes(filter) 
+            || post.body.toLowerCase().includes(filter)
+            || post.author.toLowerCase().includes(filter)
+          )
+        : this.showedPosts
     },
     nothingFound(){
-      return this.sortPosts.length === 0
+      return !this.sortPosts.length
     },
     isLoaded(){
-      return this.posts.length > 0
+      return this.showedPosts.length
     }
   },
   mounted(){
     this.getAllData()
+    window.addEventListener('scroll', () => this.loadData())
   }
 }
 </script>
+
+<style>
+.input-search{
+  width: 90% !important;
+  border-radius: 8px;
+  padding: 6px;
+  gap: 10px;
+}
+</style>
--- src/components/AppPost.vue
+++ src/components/AppPost.vue
@@ -1,18 +1,14 @@
 <template>
-
-  <div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h4 class="card-title text-primary">{{ data.title }}</h4>
-      <p class="card-text">{{ data.body }}</p>
-      <p class="card-text text-muted">{{ data.author }}</p>
-    </div>
+  <div class="card p-3">
+      <h4 class="text-primary">{{ data.title }}</h4>
+      <p>{{ data.body }}</p>
+      <p class="text-muted">{{ data.author }}</p>
   </div>
 
 </template>
 
 <script>
 export default {
-
   props: {
     data: Object
   }
